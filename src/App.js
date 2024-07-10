83eeb9c315b77e8f4a08e793e568af8423a21922
Jeremias <jeremontoya155@gmail.com>
2024-07-10 14:07:58 -0300

    netlify Subidaa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
+import HotelCatalog from './components/HotelCatalog';
+import Banner from './components/Banner';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Banner />
+        <Navbar />
       </header>
+      <main>
+        <HotelCatalog />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
